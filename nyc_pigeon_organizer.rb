70d5b1739914d64815a1ffd87d2ce9dfea3710e5
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 17:17:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,6 +18,7 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
+  #Adds the birds color to each bird. 
   x = pigeon_data.keys
   data[:color].each do |color, name|
     name.each do |bird|
