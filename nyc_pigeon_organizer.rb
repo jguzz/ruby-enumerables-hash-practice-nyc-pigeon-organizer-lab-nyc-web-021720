0e6d3f27f0cb09864827585feb26cbb889679f1b
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 16:58:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,14 @@
 require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
-
+  pigeons = {}
   data.each do |key, val|
     # key => :color
     # val => hash of color
     val.each do |k, v|
       #goes through every purple, then gender, than lives.
       binding.pry
+      []
     end
   end
 end
