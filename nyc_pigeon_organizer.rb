1d62b5f1770196c2a5b5be3034e7f13bad9dcf3d
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 16:51:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,8 @@ def nyc_pigeon_organizer(data)
   # write your code here!
 
   data.each do |key, val|
-    binding.pry
+    # key => :color
+    # val => hash of color 
+    val.each do ||
   end
 end
